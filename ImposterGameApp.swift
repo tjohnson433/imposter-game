cbd6c254c6a779ffe3b9197f5354fb5f05544655
tjohnson433 <tjohnson433@icloud.com>
2025-12-16 13:05:46 +0000

    Add files via upload

---
+++ ImposterGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImposterGameApp.swift
+//  ImposterGame
+//
+//  Created by Thomas Johnson on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ImposterGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
