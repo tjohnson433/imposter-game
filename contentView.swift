92cdb6e502e65f6c31c508760cf3685aa59cb826
tjohnson433 <tjohnson433@icloud.com>
2025-12-17 20:51:36 +0000

    Enhance ContentView with game navigation and UI updates
    Updated ContentView to include navigation links for Imposter Game and Charades with improved UI elements. Adjusted numberPicker range in imposterView and modified NavigationLink to pass selected number of players.

--- contentView.swift
+++ contentView.swift
@@ -1,14 +1,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    var body : some View {
+        @Environment(\.dismiss) var dismiss
+        NavigationStack {
+            ZStack {
+                Color.green
+                    .opacity(0.8)
+                    .ignoresSafeArea()
+                VStack {
+                    Spacer()
+                    Spacer()
+                    NavigationLink(destination: imposterView()) {
+                        Text("Imposter Game")
+                            .font(.system(size: 30, weight: .heavy, design: .rounded))
+                            .bold()
+                            .foregroundColor(.white)
+                            .frame(width: 350, height: 70)
+                            .background(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(.pink)
+                                    .shadow(radius: 5)
+                            )
+                    }
+                    .padding()
+                    Spacer()
+                    NavigationLink(destination: charadeView()) {
+                        Text("Charades")
+                            .font(.system(size: 30, weight: .heavy, design: .rounded))
+                            .bold()
+                            .foregroundColor(.white)                        
+                            .frame(width: 350, height: 70)
+                            .background(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(.yellow)
+                                    .opacity(0.9)
+                                    .shadow(radius: 5)
+                            )
+                    }
+                    .padding()
+                    Spacer()
+                    Spacer()
+                }
+                
+            }
+            
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct imposterView : View {
     @State private var numPlayers = 0
-    @State private var numberPicker = 3...10
+    @State private var numberPicker = 3...9
     @State private var numberPickerPicked = 3
     
     var body: some View {
-        NavigationStack{
+        //NavigationStack{
             ZStack {
-                BackgroundView()
+                Color.pink
+                    .ignoresSafeArea()
                 VStack {
                     Text("Number of players")
                         .font(.title2)
@@ -26,16 +77,17 @@ struct ContentView: View {
                     .pickerStyle(.wheel)
                     
                 
-                    NavigationLink(destination: playersView()) {
+                    NavigationLink(destination: playersView(numberPickerPicked: numberPickerPicked)) {
                         Text("Done")
                             .font(.system(size: 30, weight: .heavy, design: .rounded))
                             .bold()
                             .foregroundColor(.white)
+                        
+                        .frame(width: 150, height: 60)
                             .background(
                                 RoundedRectangle(cornerRadius: 8)
                                     .fill(.green)
                                     .opacity(0.9)
-                                    .frame(width: 150, height: 60)
                             )
                             .padding()
                     }
@@ -44,14 +96,8 @@ struct ContentView: View {
             .navigationBarBackButtonHidden(true)
         }
     }
-}
 
-struct BackgroundView: View {
-    var body: some View {
-        Color.pink
-            .ignoresSafeArea()
-    }
-}
+
 
 
 
